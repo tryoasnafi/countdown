2304e98ef419174243bf768f0529e6f9a49b1ace
Tryo Asnafi <tryoasnafi@gmail.com>
2022-03-28 23:15:49 +0700

    update: add button reset event

--- script.js
+++ script.js
@@ -1,38 +1,78 @@
 const hourCounterEl = document.getElementById('hours')
 const minuteCounterEl = document.getElementById('minutes')
 const secondCounterEl = document.getElementById('seconds')
+const inputHoursEl = document.getElementById('inputHours')
+const inputMinutesEl = document.getElementById('inputMinutes')
+const inputSecondsEl = document.getElementById('inputSeconds')
+
+let isTimerSet = false
+
+let counter = null
 
 const getMilliseconds = (hours, minutes, seconds) => (hours * 60 * 60) + (minutes * 60) + seconds
 
-document.addEventListener('DOMContentLoaded', (e) => {
-  e.preventDefault()
-})
-
-document.getElementById('btnSubmit').addEventListener('click', () => {
-  const hours = +document.getElementById('inputHours').value
-  const minutes = +document.getElementById('inputMinutes').value
-  const seconds = +document.getElementById('inputSeconds').value
-
-  const time = Math.ceil(+new Date() / 1000) + getMilliseconds(hours, minutes, seconds) + 1
-
-  const counter = setInterval(() => {
+const startCounter = (time) => {
+  counter = setInterval(() => {
     const currentTime = Math.ceil(+new Date() / 1000)
     const diff = time - currentTime
+
     const h = Math.floor(diff / 60 / 60) % 24
     const m = Math.floor(diff / 60) % 60
     const s = diff % 60
 
-    hourCounterEl.innerHTML = h < 10 ? `0${h}` : h
-    minuteCounterEl.innerHTML = m < 10 ? `0${m}` : m
-    secondCounterEl.innerHTML = s < 10 ? `0${s}` : s
+    console.log(diff)
+
+    inputHoursEl.setAttribute('disabled', '')
+    inputMinutesEl.setAttribute('disabled', '')
+    inputSecondsEl.setAttribute('disabled', '')
+
+    inputHoursEl.value = h < 10 ? `0${h}` : h
+    inputMinutesEl.value = m < 10 ? `0${m}` : m
+    inputSecondsEl.value = s < 10 ? `0${s}` : s
 
     if (diff <= 0) {
       clearInterval(counter)
-      alert("Selesai")
+      alert("Selesai, waktu habis!")
     }
 
   }, 1000)
+}
+
+const resetCounter = () => {
+  isTimerSet = false
+  clearInterval(counter)
+
+  inputHoursEl.removeAttribute('disabled')
+  inputMinutesEl.removeAttribute('disabled')
+  inputSecondsEl.removeAttribute('disabled')
+}
+
+
+const addTimer = () => {
+  const hours = +inputHoursEl.value
+  const minutes = +inputMinutesEl.value
+  const seconds = +inputSecondsEl.value
+
+  if (hours <= 0 && minutes <= 0 && seconds <= 0) return
+
+  const time = Math.ceil(+new Date() / 1000) + getMilliseconds(hours, minutes, seconds)
+
+  startCounter(time)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const submitFormTimer = document.getElementById('formTimer')
+  submitFormTimer.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addTimer()
+    submitFormTimer.reset()
+  })
 })
+
+document.getElementById('btnReset').addEventListener('click', () => {
+  resetCounter()
+})
+
 
 
 
